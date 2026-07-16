cd4dbb50ea515958520ff21f6ac21b25d97fcd88
Abu Jar <Abujorjoarder@gmail.com>
2026-07-16 06:47:17 +0600

    Added login system

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-// এক্সপ্রেসকে বলুন যে এই ফোল্ডারেই ফাইলগুলো আছে
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// হোম পেজ - Index.html ফাইল পাঠানো (বড় হাতের I দিয়ে)
+// লগ-ইন পেজ দেখানোর জন্য
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// লগ-ইন ফর্ম সাবমিট হলে চেক করার জন্য
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "1234") {
+        res.send("স্বাগতম! লগ-ইন সফল হয়েছে।");
+    } else {
+        res.send("ভুল ইউজারনেম বা পাসওয়ার্ড।");
+    }
+});
+
+// মেইন পেজ বা ড্যাশবোর্ড
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Index.html'));
 });
 
-// রেন্ডারের জন্য পোর্ট বাইন্ডিং
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
