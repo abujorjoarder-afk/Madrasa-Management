f9f5a4443231779991b64f5973c7369148aca7a7
Abu Jar <Abujorjoarder@gmail.com>
2026-07-16 06:32:51 +0600

    Fixed file name capitalization

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 // এক্সপ্রেসকে বলুন যে এই ফোল্ডারেই ফাইলগুলো আছে
 app.use(express.static(__dirname));
 
-// হোম পেজ - index.html ফাইল পাঠানো
+// হোম পেজ - Index.html ফাইল পাঠানো (বড় হাতের I দিয়ে)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'Index.html'));
 });
 
-// রেন্ডারের জন্য পোর্ট বাইন্ডিং (এখানেই পোর্ট একবারই ঘোষণা করা হয়েছে)
+// রেন্ডারের জন্য পোর্ট বাইন্ডিং
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
